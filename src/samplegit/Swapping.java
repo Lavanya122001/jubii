9ce53c365a2fd36a5f8e1c4fbed2d8bdec2157b1
Lavanya122001 <lavanya.mohan1208@gmail.com>
2024-09-23 13:17:08 +0530

    testing

---
+++ src/samplegit/Swapping.java
@@ -0,0 +1,14 @@
+package samplegit;
+
+public class Swapping {
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 40;
+		a = a+b;
+		b = a-b;	
+		a = a-b;
+		System.out.println(a+" "+b);
+		
+	}
+
+}
